7814a25b273c46610f33fc55bde2a0890d485216
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-03-07 00:44:25 -0500

    complete Object Lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {};
+employee.name;
+employee.streetAddress;
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updateEmployee = {...employee};
+updateEmployee[key] = value;
+    return updateEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+
+
+function deleteFromEmployeeByKey(employee ,key){
+    const tempEmployee = {...employee};
+    delete tempEmployee[key];
+    return tempEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
+
